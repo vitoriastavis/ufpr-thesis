e97db3c6a776560fbdc529dd83070dbc0a662813
vitoria stavis <vitoriastavisa@gmail.com>
2024-09-30 19:29:42 -0300

    a

--- dataset.py
+++ dataset.py
@@ -1,7 +1,6 @@
 import torch
 from torch.utils.data import Dataset
 
-
 class MyDataset(Dataset):
     def __init__(self, data, labels):
         self.data = data
@@ -10,5 +9,11 @@ class MyDataset(Dataset):
     def __len__(self):
         return len(self.data)
 
+    # def __getitem__(self, idx):
+    #     return self.data[idx], self.labels[idx]
+    
     def __getitem__(self, idx):
-        return self.data[idx], self.labels[idx]
+        # Convert to tensor 
+        embedding_tensor = torch.tensor(self.embeddings[idx], dtype=torch.float32)
+        label_tensor = torch.tensor(self.labels[idx], dtype=torch.long)  
+        return embedding_tensor, label_tensor
