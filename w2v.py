ca5ce8e47b03555cc5860d9d5594affa9d41d191
Vitoria <vitoriastavisa@gmail.com>
2024-10-01 21:25:32 -0200

    a

--- w2v.py
+++ w2v.py
@@ -6,6 +6,8 @@ from gensim.models import Word2Vec
 from nltk.corpus import stopwords
 import pandas as pd
 import numpy as np
+from transformers import AutoTokenizer
+from collections import defaultdict
 
 def w2v(model, word):   
 
@@ -14,6 +16,42 @@ def w2v(model, word):
         return model.wv[word]
     else:
         return None
+    
+def compute_pair_freqs(splits):
+    pair_freqs = defaultdict(int)
+    for word, freq in word_freqs.items():
+        split = splits[word]
+        if len(split) == 1:
+            continue
+        for i in range(len(split) - 1):
+            pair = (split[i], split[i + 1])
+            pair_freqs[pair] += freq
+    return pair_freqs
+
+def bpe(corpus):
+    tokenizer = AutoTokenizer.from_pretrained('gpt2')
+   
+
+    word_freqs = defaultdict(int)
+
+    for text in corpus:
+        words_with_offsets = tokenizer.backend_tokenizer.pre_tokenizer.pre_tokenize_str(text)
+        new_words = [word for word, offset in words_with_offsets]
+        for word in new_words:
+            word_freqs[word] += 1
+
+    alphabet = ['A', 'C', 'T', 'G']
+    vocab = ["<|endoftext|>"] + alphabet.copy()
+
+    # for word in word_freqs.keys():
+    #     for letter in word:
+    #         if letter not in alphabet:
+    #             alphabet.append(letter)
+    # alphabet.sort()
+
+    splits = {word: [c for c in word] for word in word_freqs.keys()}
+
+
 
 # Função para ler o CSV e gerar os encodings
 def process_csv(train_file, encode_file):
